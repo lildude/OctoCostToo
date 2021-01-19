f7fa8cec9b7b7d277079becf51e65eaa59c7d273
Colin Seymour <colin@symr.io>
2021-01-19 17:29:32 +0000

    Add initial test for calculate_cost_and_usage

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,3 +22,23 @@ def fixture_mock_elec_meter_points(requests_mock):
     "https://api.octopus.energy/v1/electricity-meter-points/12345",
     text= open("tests/fixtures/elec-meter-points.json", "r").read()
   )
+
+@pytest.fixture(name="mock_elec_consumption_one_day")
+def fixture_mock_elec_consumption_one_day(requests_mock):
+  """
+  Mock response for a single day's electricity usage
+  """
+  requests_mock.get(
+    "https://api.octopus.energy/v1/electricity-meter-points/12345/meters/67890/consumption/?order_by=period&period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z&page_size=47",
+    text= open("tests/fixtures/one-day-usage.json", "r").read()
+  )
+
+@pytest.fixture(name="mock_elec_agile_cost_one_day")
+def fixture_mock_elec_agile_cost_one_day(requests_mock):
+  """
+  Mock response for a single day's Agile electricity prices
+  """
+  requests_mock.get(
+    "https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-H/standard-unit-rates/?period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z",
+    text= open("tests/fixtures/agile-cost-one-day.json", "r").read()
+  )
--- tests/test_octocost.py
+++ tests/test_octocost.py
@@ -8,6 +8,11 @@ import json
 @automation_fixture(
   OctoCost,
   args= {
+    "auth": "sk_live_abcdefghijklmnopqrstuvwxyz",
+    "mpan": "12345",
+    "region": "H",
+    "serial": "67890",
+    "start_date": "2020-12-27",
   },
   initialize=True
 )
@@ -27,4 +32,15 @@ def test_calculate_count(octocost: OctoCost):
     start_day = datetime.date(2020, 1, 18)
     # Two day's worth of 30 minute windows between start and end of the same day
     assert 95 == octocost.calculate_count(start_day)
+
+@pytest.mark.usefixtures("mock_elec_consumption_one_day", "mock_elec_agile_cost_one_day")
+def test_calculate_cost_and_usage(octocost: OctoCost):
+    octocost.yesterday = datetime.date(2021, 1, 18)
+    octocost.use_url = octocost.elec_consumption_url
+    octocost.gas = False
+    start_day = datetime.date(2021, 1, 18)
+
+    usage, cost = octocost.calculate_cost_and_usage(start_day)
+    assert usage == 7.701
+    assert cost == 108.6
 
