1dd5b5673ac4c41c0d67d835afe59d5a5eff6e4e
Colin Seymour <colin@symr.io>
2021-01-20 17:26:19 +0000

    Add mocks for gas

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,3 +42,27 @@ def fixture_mock_elec_agile_cost_one_day(requests_mock):
     "https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-H/standard-unit-rates/?period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z",
     text= open("tests/fixtures/agile-cost-one-day.json", "r").read()
   )
+
+@pytest.fixture(name="mock_gas_consumption_one_day")
+def fixture_mock_gas_consumption_one_day(requests_mock):
+  """
+  Mock response for a single day's gas usage
+  """
+  requests_mock.get(
+    "https://api.octopus.energy/v1/gas-meter-points/54321/meters/98765/consumption/?order_by=period&period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z&page_size=47",
+    text= open("tests/fixtures/elec-one-day-usage.json", "r").read()
+  )
+
+@pytest.fixture(name="mock_gas_rates")
+def fixture_mock_gas_rates(requests_mock):
+  """
+  Mock response for gas standard unit rates & standing charge
+  """
+  requests_mock.get(
+    "https://api.octopus.energy/v1/products/FIX-12M-20-09-21/gas-tariffs/G-1R-FIX-12M-20-09-21-H/standard-unit-rates/?period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z",
+    text= open("tests/fixtures/gas-standard-rate.json", "r").read()
+  )
+  requests_mock.get(
+    "https://api.octopus.energy/v1/products/FIX-12M-20-09-21/gas-tariffs/G-1R-FIX-12M-20-09-21-H/standing-charges/?period_from=2021-01-18T00:00:00Z&period_to=2021-01-18T23:59:59Z",
+    text= open("tests/fixtures/gas-standing-charges.json", "r").read()
+  )
